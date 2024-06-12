e0028b5f881af3c73c5644e2f0158ec30fabea93
Sayrinix <sa.luciano@duocuc.cl>
2024-06-11 23:10:20 -0400

    docs

---
+++ pikachu-sayrina/assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#ojo1").slideToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#ojo2").slideToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#cola").fadeToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#brazo1").fadeToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#brazo2").fadeToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#boca").slideToggle(); 
+    });
+});
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+        $("#cuerpo").fadeToggle(); 
+    });
+});
